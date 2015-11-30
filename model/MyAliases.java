7c936989af197b006424fb3e64c69da4f345e59d
pierre <rpierre@localhost.localdomain>
2015-11-30 18:15:11 +0100

    Add MyAliases class

---
+++ model/MyAliases.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyAliases {
+
+	Map<Integer, String> mapTuiles = new HashMap<Integer, String> ();
+
+	Map<Integer, String> mapSommets = new HashMap<Integer, String> ();
+
+}
