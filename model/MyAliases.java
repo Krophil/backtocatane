aaec8d8a94545e7fd717d120b0b73b30ce10a86a
pierre <rpierre@localhost.localdomain>
2015-11-30 19:51:08 +0100

    classe MyAlias commencé

--- model/MyAliases.java
+++ model/MyAliases.java
@@ -7,6 +7,38 @@ public class MyAliases {
 
 	Map<Integer, String> mapTuiles = new HashMap<Integer, String> ();
 
+
 	Map<Integer, String> mapSommets = new HashMap<Integer, String> ();
 
+
+	public MyAliases(){
+		//Table de correspondance des tuiles int <-> String
+		//a faire
+		mapTuiles.put(0, "A");
+		mapTuiles.put(1, "B");
+		mapTuiles.put(2, "C");
+		mapTuiles.put(3, "D");
+		mapTuiles.put(4, "E");
+		mapTuiles.put(5, "F");
+		mapTuiles.put(6, "G");
+		mapTuiles.put(7, "H");
+		mapTuiles.put(8, "I");
+		mapTuiles.put(9, "J");
+		mapTuiles.put(10,"K");
+		mapTuiles.put(11,"L");
+		mapTuiles.put(12,"M");
+		mapTuiles.put(13,"N");
+		mapTuiles.put(14,"O");
+		mapTuiles.put(15,"P");
+		mapTuiles.put(16,"Q");
+		mapTuiles.put(17,"R");
+		mapTuiles.put(18,"S");
+
+		//Table de correspondance des sommets int <-> String
+		mapSommets.put(0,)
+
+
+
+
+	}
 }
